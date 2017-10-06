7f0a84a14bf4d37ee43bb173305fb617fdd2fc36
Elai <ellyza.pepito@gmail.com>
2017-10-07 00:14:02 +0800

    wcf test

---
+++ WcfTest/WcfTest/IMyGameService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfTest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMyGameService
+    {
+        [OperationContract]
+        void AddGame(Game g);
+
+
+        [OperationContract]
+        List<Game> GetAllGames();
+
+
+        [OperationContract]
+        Game GetGameByID(int id);
+
+    }
+
+    [DataContract]
+    public class Game
+    {
+        [DataMember]
+        public int ID { set; get; }
+        [DataMember]
+        public string Name { set; get; }
+        [DataMember]
+        public string Publisher { set; get; }
+        public Game() { }
+    }
+
+}
---
+++ WcfTest/WcfTest/MyGameService.svc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfTest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IMyGameService
+    {
+        static List<Game> _games = new List<Game>();
+        static object locker = new object();
+
+
+        #region IMyGameService Members 
+
+
+        public void AddGame(Game g)
+        {
+            g.ID = GetNextID();
+            lock (locker)
+            {
+                _games.Add(g);
+            }
+        }
+
+
+        public List<Game> GetAllGames()
+        {
+            return _games;
+        }
+
+
+        public Game GetGameByID(int id)
+        {
+            lock (locker)
+            {
+                foreach (Game g in _games)
+                {
+                    if (g.ID == id)
+                        return g;
+                }
+            }
+            return null;
+        }
+
+
+        #endregion
+
+
+        private int GetNextID()
+        {
+            int maxID = 0;
+            lock (locker)
+            {
+                foreach (Game g in _games)
+                {
+                    if (g.ID > maxID)
+                        maxID = g.ID;
+                }
+            }
+            return maxID + 1;
+        }
+
+    }
+}
